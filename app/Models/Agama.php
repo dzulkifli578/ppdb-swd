213d66123663a3c72fcd618326101376edf53a9c
da578 <dzulkiflianwar2@gmail.com>
2024-09-22 19:22:57 +0700

    feat: add Agama model for `peserta` registration form
    - Added `Agama` model to manage religious data in the student registration form for PPDB.
    - Defined table `agama`.
    - Set fillable fields including `nama`.
    - Enabled timestamps for `created_at` and `updated_at`.

---
+++ app/Models/Agama.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Agama extends Model
+{
+    protected $table = 'agama';
+
+    protected $fillable = [
+        'nama',
+    ];
+
+    public $timestamps = true;
+}
