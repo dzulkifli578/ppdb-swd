1536b8518f2682c9d1f1fc3b152387e6a532f297
da578 <dzulkiflianwar2@gmail.com>
2024-09-22 19:42:34 +0700

    feat: overhaul registration form structure and design
    - Enhanced form layout by dividing it into three sections for better organization.
    - Added new fields: Tempat Lahir, Tanggal Lahir, Jenis Kelamin, and Agama.
    - Updated labels and placeholders for clarity and guidance.
    - Implemented required attributes for necessary fields to ensure data integrity.
    - Improved visual styling with hover effects on header for better user experience.
    - Used Blade directives to dynamically populate dropdowns for jurusan.

--- resources/views/formulir-pendaftaran.blade.php
+++ resources/views/formulir-pendaftaran.blade.php
@@ -14,61 +14,155 @@
     @include('navbar')
 
     <!-- Header -->
-    <header class="bg-base-300 flex flex-col justify-center items-center rounded-xl shadow-xl mx-6 my-5 p-5">
-        <h1 class="text-4xl font-bold">Formulir Pendaftaran</h1>
+    <header
+        class="group bg-base-300 flex flex-col justify-center items-center rounded-xl shadow-xl mx-6 my-6 p-6 gap-y-6 hover:bg-warning ease-in-out duration-300">
+        <h1
+            class="font-bold text-center text-2xl md:text-3xl lg:text-4xl group-hover:text-warning-content ease-in-out duration-300">
+            Formulir Pendaftaran</h1>
     </header>
 
     <!-- Content -->
     <content>
-        <form action="{{ route('proses-formulir-pendaftaran') }}" method="post"
-            class="container mx-auto bg-base-300 rounded-xl shadow-xl p-5 mb-5">
-            @csrf
-            <div class="grid grid-cols-2 gap-x-5">
-                <!-- Nama Lengkap -->
-                <label class="form-control w-full">
-                    <div class="label">
-                        <span class="label-text">Nama Lengkap</span>
-                    </div>
-                    <input type="text" placeholder="Masukkan nama..." class="input input-bordered input-accent" />
-                </label>
-                <!-- Email -->
-                <label class="form-control w-full">
-                    <div class="label">
-                        <span class="label-text">Email</span>
-                    </div>
-                    <input type="email" placeholder="Masukkan email..." class="input input-bordered input-accent" />
-                </label>
-                <!-- No. HP -->
-                <label class="form-control w-full">
-                    <div class="label">
-                        <span class="label-text">No. HP</span>
-                    </div>
-                    <input type="phone" placeholder="Masukkan no. Hp..." class="input input-bordered input-accent" />
-                </label>
-                <!-- Alamat -->
-                <label class="form-control w-full">
-                    <div class="label">
-                        <span class="label-text">Alamat</span>
-                    </div>
-                    <input type="text" placeholder="Masukkan alamat" class="input input-bordered input-accent" />
-                </label>
-                <!-- Program Keahlian -->
-                <label class="form-control w-full col-span-2">
-                    <div class="label">
-                        <span class="label-text">Program Keahlian</span>
-                    </div>
-                    <select class="select select-bordered select-accent">
-                        <option disabled selected>Pilih salah satu</option>
-                        <option>Rekayasa Perangkat Lunak</option>
-                        <option>Teknik Komputer dan Jaringan</option>
-                        <option>Teknik Instalasi Tenaga Listrik</option>
-                        <option>Teknik Kendaraan Ringan</option>
-                        <option>Teknik dan Bisnis Sepeda Motor</option>
-                    </select>
-                </label>
-                <button type="submit" class="btn btn-primary font-bold col-span-2 mt-6">Submit</button>
-            </div>
-        </form>
+        <div class="flex flex-col gap-y-6 my-6 mx-6">
+            <form action="{{ route('proses-formulir-pendaftaran') }}" method="post"
+                class="bg-base-300 rounded-xl shadow-xl p-6">
+                @csrf
+                <div class="grid grid-cols-2 gap-x-5 gap-y-2">
+                    <!-- Bagian 1 -->
+                    <h2 class="col-span-2 font-bold text-2xl md:text-3xl my-2">Bagian 1</h2>
+                    <!-- Nama Lengkap -->
+                    <label class="form-control w-full">
+                        <div class="label">
+                            <span class="label-text">Nama Lengkap</span>
+                        </div>
+                        <input type="text" name="nama" placeholder="Masukkan nama lengkap..."
+                            class="input input-bordered input-accent" required />
+                    </label>
+                    <!-- Tempat Lahir -->
+                    <label class="form-control w-full">
+                        <div class="label">
+                            <span class="label-text">Tempat Lahir</span>
+                        </div>
+                        <input type="text" name="tempat_lahir" placeholder="Masukkan tempat lahir..."
+                            class="input input-bordered input-accent" required />
+                    </label>
+                    <!-- Tanggal Lahir -->
+                    <label class="form-control w-full">
+                        <div class="label">
+                            <span class="label-text">Tanggal Lahir</span>
+                        </div>
+                        <input type="date" name="tanggal_lahir" placeholder="Masukkan tanggal lahir..."
+                            class="input input-bordered input-accent" required />
+                    </label>
+                    <!-- Jenis Kelamin -->
+                    <label class="form-control w-full">
+                        <div class="label">
+                            <span class="label-text">Jenis Kelamin</span>
+                        </div>
+                        <select name="jenis_kelamin" class="select select-bordered select-accent" required>
+                            <option disabled selected>Pilih salah satu</option>
+                            <option value="Laki-laki">Laki-laki</option>
+                            <option value="Perempuan">Perempuan</option>
+                        </select>
+                    </label>
+                    <!-- Agama -->
+                    <label class="form-control w-full">
+                        <div class="label">
+                            <span class="label-text">Agama</span>
+                        </div>
+                        <select name="agama" class="select select-bordered select-accent" required>
+                            <option disabled selected>Pilih salah satu</option>
+                            <option value="Islam">Islam</option>
+                            <option value="Kristen Katolik">Kristen Katolik</option>
+                            <option value="Kristen Protestan">Kristen Protestan</option>
+                            <option value="Hindu">Hindu</option>
+                            <option value="Buddha">Buddha</option>
+                            <option value="Khonghucu">Khonghucu</option>
+                        </select>
+                    </label>
+                    <!-- Alamat -->
+                    <label class="form-control w-full">
+                        <div class="label">
+                            <span class="label-text">Alamat</span>
+                        </div>
+                        <input type="text" name="alamat" placeholder="Masukkan alamat..."
+                            class="input input-bordered input-accent" required />
+                    </label>
+
+                    <!-- Bagian 2 -->
+                    <h2 class="col-span-2 font-bold text-2xl md:text-3xl my-2">Bagian 2</h2>
+                    <!-- Asal Sekolah -->
+                    <label class="form-control w-full col-span-2">
+                        <div class="label">
+                            <span class="label-text">Asal Sekolah</span>
+                        </div>
+                        <input type="text" name="asal_sekolah" placeholder="Masukkan asal sekolah..."
+                            class="input input-bordered input-accent" required />
+                    </label>
+                    <!-- Pilihan Jurusan Pertama -->
+                    <label class="form-control w-full">
+                        <div class="label">
+                            <span class="label-text">Pilihan Jurusan Pertama</span>
+                        </div>
+                        <select name="jurusan_pertama_id" class="select select-bordered select-accent" required>
+                            <option disabled selected>Pilih salah satu</option>
+                            @foreach ($jurusan as $item)
+                                <option value="{{ $item->id }}">{{ $item->nama }}</option>
+                            @endforeach
+                        </select>
+                    </label>
+                    <!-- Pilihan Jurusan Kedua -->
+                    <label class="form-control w-full">
+                        <div class="label">
+                            <span class="label-text">Pilihan Jurusan Kedua</span>
+                        </div>
+                        <select name="jurusan_kedua_id" class="select select-bordered select-accent" required>
+                            <option disabled selected>Pilih salah satu</option>
+                            @foreach ($jurusan as $item)
+                                <option value="{{ $item->id }}">{{ $item->nama }}</option>
+                            @endforeach
+                        </select>
+                    </label>
+
+                    <!-- Bagian 3 -->
+                    <h2 class="col-span-2 font-bold text-2xl md:text-3xl my-2">Bagian 3</h2>
+                    <!-- Nama Orang Tua / Wali -->
+                    <label class="form-control w-full">
+                        <div class="label">
+                            <span class="label-text">Nama Orang Tua / Wali</span>
+                        </div>
+                        <input type="text" name="nama_ortu" placeholder="Masukkan nama orang tua / wali..."
+                            class="input input-bordered input-accent" required />
+                    </label>
+                    <!-- Alamat Orang Tua / Wali -->
+                    <label class="form-control w-full">
+                        <div class="label">
+                            <span class="label-text">Alamat Orang Tua / Wali</span>
+                        </div>
+                        <input type="text" name="alamat_ortu" placeholder="Masukkan alamat orang tua / wali..."
+                            class="input input-bordered input-accent" required />
+                    </label>
+                    <!-- Pekerjaan -->
+                    <label class="form-control w-full">
+                        <div class="label">
+                            <span class="label-text">Pekerjaan</span>
+                        </div>
+                        <input type="text" name="pekerjaan_ortu"
+                            placeholder="Masukkan pekerjaan orang tua / wali..."
+                            class="input input-bordered input-accent" required />
+                    </label>
+                    <!-- No. Telepon Siswa -->
+                    <label class="form-control w-full">
+                        <div class="label">
+                            <span class="label-text">No. Telepon Siswa</span>
+                        </div>
+                        <input type="tel" name="no_telepon" placeholder="Masukkan no. telepon siswa..."
+                            class="input input-bordered input-accent" required />
+                    </label>
+                    <button type="submit" class="btn btn-primary font-bold col-span-2 mt-6">Submit</button>
+                </div>
+            </form>
+        </div>
     </content>
 
     <!-- Footer -->
