f52f7a5e672864836da67a70de508ae1d52b94f5
da578 <dzulkiflianwar2@gmail.com>
2024-09-22 19:41:52 +0700

    feat: enhance footer design and content
    - Updated footer layout with improved styling and structure.
    - Added school name and tagline for better branding.
    - Included contact icons for phone, location, and email for accessibility.
    - Increased font sizes for better readability across devices.

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -1,5 +1,14 @@
 <footer class="footer footer-center bg-base-300 text-base-content p-4">
-    <aside>
-        <p class="text-center">Copyright © {{ date('Y') }} - All right reserved by SMK Swadhipa 2 Natar</p>
+    <aside class="flex flex-col justify-center gap-y-5">
+        <p class="font-semibold text-center text-lg md:text-xl lg:text-2xl">SMK Swadhipa 2 Natar</p>
+        <p class="text-medium">Teknologi Rekayasa dan Teknologi Informasi Komunikasi.</p>
+        <div class="flex flex-row justify-center gap-2">
+            <a href="#"><img src="{{ asset('img/icon/phone.svg') }}" alt="Phone">
+            </a>
+            <a href="#"><img src="{{ asset('img/icon/map-pin.svg') }}" alt="Phone">
+            </a>
+            <a href="#"><img src="{{ asset('img/icon/mail.svg') }}" alt="Phone">
+            </a>
+        </div>
     </aside>
 </footer>
