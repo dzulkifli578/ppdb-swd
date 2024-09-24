285f86ae7eb82878f37a1531bc9ed71ad51f03fc
da578 <dzulkiflianwar2@gmail.com>
2024-09-24 17:51:21 +0700

    feat: adds responsiveness and makes code cleaner

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -15,33 +15,24 @@
     @include('admin.navbar')
 
     <!-- Header -->
-    <header class="bg-base-300 flex flex-row justify-between items-center rounded-xl shadow-xl mx-6 my-5 p-5">
-        <h1 class="text-4xl font-bold">Dashboard Admin</h1>
-        <div class="breadcrumbs text-sm">
-            <ul>
-                <li><a href="{{ route('admin-dashboard') }}">Dashboard</a></li>
-            </ul>
-        </div>
-    </header>
+    @include('admin.components.header')
 
     <!-- Content -->
     <content>
-        <div class="bg-base-300 flex flex-col justify-center items-center rounded-xl shadow-xl mx-6 my-6 p-6">
-            <div class="container mx-auto">
-                <div class="flex flex-row justify-between items-center mb-5">
-                    <h2 class="text-2xl font-bold">Data Pendaftar</h2>
-                    <form action="{{ route('import-csv') }}" method="post" enctype="multipart/form-data"
-                        class="flex flex-row items-center">
-                        @csrf
-                        <input type="file" accept=".csv" name="csv_file"
-                            class="file-input file-input-bordered file-input-primary w-full max-w-xs mr-2" required />
-                        <button type="submit" class="btn btn-primary">Submit</button>
-                    </form>
-                </div>
+        <div class="bg-base-300 flex flex-col justify-center items-center rounded-xl shadow-xl mx-6 my-6 p-6 gap-y-6">
+            <div class="w-full flex flex-row justify-between items-center">
+                <h2 class="text-base font-bold md:text-xl lg:text-2xl mr-6">Data Pendaftar</h2>
+                <form action="{{ route('import-csv') }}" method="post" enctype="multipart/form-data"
+                    class="flex flex-row items-center">
+                    @csrf
+                    <input type="file" accept=".csv" name="csv_file"
+                        class="file-input file-input-bordered file-input-primary w-full max-w-xs mr-2" required />
+                    <button type="submit" class="btn btn-primary">Submit</button>
+                </form>
             </div>
             <!-- Search -->
-            <div class="container mx-auto my-5">
-                <label class="input input-bordered flex items-center gap-2 ">
+            <div class="w-full flex flex-col justify-between items-center">
+                <label class="input input-bordered flex items-center w-full gap-2">
                     <input type="text" id="searchInput" class="grow" placeholder="Cari berdasarkan nama..." />
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor"
                         class="h-4 w-4 opacity-70">
@@ -50,61 +41,60 @@
                             clip-rule="evenodd" />
                     </svg>
                 </label>
+            </div>
 
-                <!-- Table -->
-                <div class="container overflow-x-auto mt-5">
-                    <table class="table bg-base-100 w-full">
-                        <thead>
+            <!-- Table -->
+            <div class="flex w-full overflow-x-auto">
+                <table class="table bg-base-100">
+                    <thead>
+                        <tr>
+                            <th>ID</th>
+                            <th>Nama</th>
+                            <th>NISN</th>
+                            <th>Program Keahlian</th>
+                            <th>Detail</th>
+                            <th>Aksi</th>
+                        </tr>
+                    </thead>
+                    <tbody id="dataTable">
+                        @foreach ($registration as $item)
                             <tr>
-                                <th>ID</th>
-                                <th>Nama</th>
-                                <th>NISN</th>
-                                <th>Program Keahlian</th>
-                                <th>Detail</th>
-                                <th>Aksi</th>
-                            </tr>
-                        </thead>
-                        <tbody id="dataTable">
-                            @foreach ($registration as $item)
-                                <tr>
-                                    <td>{{ $item->id }}</td>
-                                    <td>{{ $item->name }}</td>
-                                    <td>{{ $item->nisn }}</td>
-                                    <td>{{ $item->program }}</td>
-                                    <td><button
-                                            onclick="window.location.href='{{ route('detail-peserta', ['id' => $item->id]) }}'"
-                                            class="btn btn-info font-medium"><svg xmlns="http://www.w3.org/2000/svg"
-                                                fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                                                stroke="currentColor" class="size-6">
+                                <td>{{ $item->id }}</td>
+                                <td>{{ $item->name }}</td>
+                                <td>{{ $item->nisn }}</td>
+                                <td>{{ $item->program }}</td>
+                                <td><button
+                                        onclick="window.location.href='{{ route('detail-peserta', ['id' => $item->id]) }}'"
+                                        class="btn btn-info font-medium"><svg xmlns="http://www.w3.org/2000/svg"
+                                            fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
+                                            class="size-6">
+                                            <path stroke-linecap="round" stroke-linejoin="round"
+                                                d="M2.036 12.322a1.012 1.012 0 0 1 0-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178Z" />
+                                            <path stroke-linecap="round" stroke-linejoin="round"
+                                                d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+                                        </svg>
+                                        Detail</button></td>
+                                <td>
+                                    <form action="{{ route('hapus-peserta', ['id' => $item->id]) }}" method="post">
+                                        @csrf
+                                        <button type="submit" class="btn btn-error font-medium"><svg
+                                                xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                                stroke-width="1.5" stroke="currentColor" class="size-6">
                                                 <path stroke-linecap="round" stroke-linejoin="round"
-                                                    d="M2.036 12.322a1.012 1.012 0 0 1 0-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178Z" />
-                                                <path stroke-linecap="round" stroke-linejoin="round"
-                                                    d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+                                                    d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
                                             </svg>
-                                            Detail</button></td>
-                                    <td>
-                                        <form action="{{ route('hapus-peserta', ['id' => $item->id]) }}" method="post">
-                                            @csrf
-                                            <button type="submit" class="btn btn-error font-medium"><svg
-                                                    xmlns="http://www.w3.org/2000/svg" fill="none"
-                                                    viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
-                                                    class="size-6">
-                                                    <path stroke-linecap="round" stroke-linejoin="round"
-                                                        d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
-                                                </svg>
-                                                Hapus</button>
-                                        </form>
-                                    </td>
-                                </tr>
-                            @endforeach
-                        </tbody>
-                    </table>
-                </div>
+                                            Hapus</button>
+                                    </form>
+                                </td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
             </div>
 
             <!-- PDF dan Print -->
-            <div class="container mx-auto">
-                <form action="{{ route('pdf') }}" method="post" class="flex flex-row justify-end mt-5">
+            <div class="w-full flex flex-row justify-end items-center">
+                <form action="{{ route('pdf') }}" method="post">
                     @csrf
                     <button @click="{{ route('pdf') }}" class="btn btn-error font-medium mr-4"><svg
                             xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
