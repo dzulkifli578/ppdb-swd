ce5ce7188e6057b37cf45838e4a36544ae262307
da578 <dzulkiflianwar2@gmail.com>
2024-09-25 08:22:19 +0700

    feat: add text responsiveness to the start navbar
    - Hide the logout button on the small screen and display it in the `lainnya` menu.

--- resources/views/peserta/navbar.blade.php
+++ resources/views/peserta/navbar.blade.php
@@ -3,12 +3,12 @@
         <div class="navbar-start">
             <a class="flex flex-row items-center ml-4 cursor-pointer">
                 <img src="{{ asset('img/swadhipa.png') }}" alt="Logo Swadhipa" class="h-12 mr-4">
-                <p class="text-xl font-semibold">SMK Swadhipa 2 Natar</p>
+                <p class="text-nowrap text-base md:text-xl lg:text-2xl font-semibold">SMK Swadhipa 2 Natar</p>
             </a>
         </div>
         <div class="navbar-end">
             <ul class="menu menu-horizontal px-1 font-semibold">
-                <li>
+                <li class="hidden sm:block">
                     <form action="{{ route('peserta-logout') }}" method="post">
                         @csrf
                         <button type="submit">Logout</button>
@@ -19,6 +19,12 @@
                         <summary>Lainnya</summary>
                         <ul class="bg-base-100 rounded-t-none p-2">
                             <li><a href="{{ route('peserta-dashboard') }}">Beranda</a></li>
+                            <li class="sm:hidden">
+                                <form action="{{ route('admin-logout') }}" method="post">
+                                    @csrf
+                                    <button type="submit">Logout</button>
+                                </form>
+                            </li>
                     </details>
                 </li>
             </ul>
