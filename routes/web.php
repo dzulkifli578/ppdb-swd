67fa427b3670bcc793f08cc8d9023b70e26edb95
da578 <dzulkiflianwar2@gmail.com>
2024-09-28 14:13:45 +0700

    feat: add a number of routes for each role's features
    - Add account routes in admin scope.
    - Add department route in admin scope.
    - And some renaming for semantic purposes.

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,8 @@
 <?php
 
-use App\Http\Controllers\AdminController;
+use App\Http\Controllers\Admin\AdminController;
 use App\Http\Controllers\LoginController;
-use App\Http\Controllers\PesertaController;
+use App\Http\Controllers\Peserta\PesertaController;
 use App\Http\Controllers\RootController;
 use App\Http\Middleware\AdminMiddleware;
 use App\Http\Middleware\PeranMiddleware;
@@ -42,32 +42,42 @@ Route::prefix("/peran/admin")->middleware(AdminMiddleware::class)->group(functio
         Route::post("/hapus/{id}", [AdminController::class, "hapusPeserta"])->name("hapus-peserta");
     });
 
+    // akun
+    Route::get("/akun", [AdminController::class, "akun"])->name("admin-akun");
+    Route::put("/akun/edit/{id}", [AdminController::class, "editAkun"])->name("edit-akun");
+
     // pengumuman
-    Route::get("/pengumuman", [AdminController::class, "pengumuman"])->name("pengumuman");
+    Route::get("/pengumuman", [AdminController::class, "pengumuman"])->name("admin-pengumuman");
     Route::post("/tambah-pengumuman", [AdminController::class, "tambahPengumuman"])->name("tambah-pengumuman");
     Route::put("/edit-pengumuman/{id}", [AdminController::class, "editPengumuman"])->name("edit-pengumuman");
     Route::delete("/hapus-pengumuman/{id}", [AdminController::class, "hapusPengumuman"])->name("hapus-pengumuman");
 
     // data peserta
-    Route::get("/peserta", [AdminController::class, "dataPeserta"])->name("data-peserta");
+    Route::get("/peserta", [AdminController::class, "dataPeserta"])->name("admin-data-peserta");
     Route::get("/tambah-peserta", [AdminController::class, "tambahPeserta"])->name("tambah-peserta");
     Route::post("/proses-tambah-peserta", [AdminController::class, "prosesTambahPeserta"])->name("proses-tambah-peserta");
     Route::get("/edit-peserta/{id}", [AdminController::class, "editPeserta"])->name("edit-peserta");
     Route::put("/proses-edit-peserta/{id}", [AdminController::class, "prosesEditPeserta"])->name("proses-edit-peserta");
     Route::delete("/hapus-peserta/{id}", [AdminController::class, "hapusPeserta"])->name("hapus-peserta");
+
+    // jurusan
+    Route::get("/jurusan", [AdminController::class, "jurusan"])->name("admin-jurusan");
+    Route::post("/tambah-jurusan", [AdminController::class, "tambahJurusan"])->name("tambah-jurusan");
+    Route::put("/edit-jurusan/{id}", [AdminController::class, "editJurusan"])->name("edit-jurusan");
+    Route::delete("/hapus-jurusan/{id}", [AdminController::class, "hapusJurusan"])->name("hapus-jurusan");
 });
 
 // peserta
 Route::prefix("/peran/peserta")->middleware(PesertaMiddleware::class)->group(function () {
     Route::get("/dashboard", [PesertaController::class, "dashboard"])->name("peserta-dashboard");
     Route::post("/logout", [PesertaController::class, "logout"])->name("peserta-logout");
-    Route::get("/biodata/", [PesertaController::class, "biodataPeserta"])->name("biodata-peserta");
+    Route::get("/biodata/", [PesertaController::class, "biodataPeserta"])->name("peserta-biodata");
     Route::post("/biodata/proses/{id}", [PesertaController::class, "prosesBiodataPeserta"])->name("proses-biodata-peserta");
-    Route::get("/status/", [PesertaController::class, "statusPenerimaanPeserta"])->name("status-penerimaan-peserta");
-    Route::get("/pengumuman/", [PesertaController::class, "pengumumanPeserta"])->name("pengumuman-peserta");
+    Route::get("/status/", [PesertaController::class, "statusPenerimaanPeserta"])->name("peserta-status-penerimaan");
+    Route::get("/pengumuman/", [PesertaController::class, "pengumuman"])->name("peserta-pengumuman");
 });
 
 // test
 Route::get("/test", function () {
-    return view("admin.pdf");
+    return view("admin.data-peserta.pdf");
 })->name("test");
