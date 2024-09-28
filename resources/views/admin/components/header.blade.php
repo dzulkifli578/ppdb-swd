852acab4b2e8a90963b0cb7e6d274b6727c14b30
da578 <dzulkiflianwar2@gmail.com>
2024-09-28 19:40:48 +0700

    refactor: change header component in admin scope to be more dynamic and modular

--- resources/views/admin/components/header.blade.php
+++ resources/views/admin/components/header.blade.php
@@ -1,8 +1,14 @@
-<header class="bg-base-300 flex flex-row justify-between items-center rounded-xl shadow-xl mx-6 my-6 p-6">
+<header class="bg-base-300 flex flex-row justify-between items-center rounded-xl shadow-xl m-6 p-6">
     <h1 class="text-xl md:text-2xl lg:text-3xl font-bold mr-6">{{ $header['name'] }}</h1>
     <div class="breadcrumbs text-sm">
         <ul>
-            <li><a href="{{ $header['route'] }}">{{ $header['breadcrumbs'] }}</a></li>
+            @if (is_array($header['breadcrumbs']))
+                @foreach ($header['breadcrumbs'] as $breadcrumb)
+                    <li><a href="{{ $breadcrumb['route'] }}">{{ $breadcrumb['label'] }}</a></li>
+                @endforeach
+            @else
+                <li><a href="{{ $header['route'] }}">{{ $header['breadcrumbs'] }}</a></li>
+            @endif
         </ul>
     </div>
 </header>
